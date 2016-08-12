e214b1cf12d3cffba1c734e45fc29ff288862ad1
Javier Sanz <javi95sanz@gmail.com>
2016-08-12 12:27:15 +0200

    Launcher ready!

--- app/src/main/java/app/javiersanz/com/androidlauncher/NerdLauncherFragment.java
+++ app/src/main/java/app/javiersanz/com/androidlauncher/NerdLauncherFragment.java
@@ -1,21 +1,33 @@
 package app.javiersanz.com.androidlauncher;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
- * Created by Usuario on 06/08/2016.
+ * Created by TibiaZ on 06/08/2016.
  */
 public class NerdLauncherFragment extends Fragment {
 
     // Variables
 
     private RecyclerView mRecyclerView;
+    private static final String TAG = "NerdLauncherFragment";
 
 
     public static NerdLauncherFragment newInstance(){
@@ -32,6 +44,95 @@ public class NerdLauncherFragment extends Fragment {
                 .findViewById(R.id.fragment_nerd_launcher_recycler_view);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
+        setupAdapter();
         return v;
+    }
+
+    // setupAdapter method
+    // It creates a RecyclerView.Adapter instance and set it on you RecyclerView object.
+    // It also creates an implicit intent and get a list of activities that match the intent from the PackageManager
+
+    private void setupAdapter(){
+        Intent startupIntent = new Intent(Intent.ACTION_MAIN);
+        startupIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+        PackageManager pm = getActivity().getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(startupIntent, 0);
+        Collections.sort(activities, new Comparator<ResolveInfo>() {
+            @Override
+            public int compare(ResolveInfo a, ResolveInfo b) {
+                PackageManager pm = getActivity().getPackageManager();
+                return String.CASE_INSENSITIVE_ORDER.compare(
+                        a.loadLabel(pm).toString(),
+                        b.loadLabel(pm).toString()
+                );
+            }
+        } );
+
+        Log.i(TAG, "Found " + activities.size() + " activities");
+        mRecyclerView.setAdapter(new ActivityAdapter(activities));
+    }
+
+    private class ActivityHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+        private ResolveInfo mResolveInfo;
+        private TextView mNameTextView;
+
+        public ActivityHolder(View itemView){
+            super(itemView);
+            mNameTextView = (TextView) itemView;
+            mNameTextView.setOnClickListener(this);
+        }
+
+        public void binActivity(ResolveInfo resolveInfo){
+            mResolveInfo = resolveInfo;
+            PackageManager pm = getActivity().getPackageManager();
+            String appName = mResolveInfo.loadLabel(pm).toString();
+            mNameTextView.setText(appName);
+        }
+
+        // onClick
+
+        @Override
+        public void onClick(View v){
+            ActivityInfo activityInfo = mResolveInfo.activityInfo;
+
+            Intent i = new Intent(Intent.ACTION_MAIN)
+                    .setClassName(activityInfo.applicationInfo.packageName,
+                            activityInfo.name)
+                    .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+            startActivity(i);
+        }
+
+    }
+
+
+    // Activity Adapter class
+
+    private class ActivityAdapter extends RecyclerView.Adapter<ActivityHolder>{
+        private final List<ResolveInfo> mActivities;
+
+        public ActivityAdapter(List<ResolveInfo> activities){
+            mActivities = activities;
+        }
+
+        @Override
+        public ActivityHolder onCreateViewHolder(ViewGroup parent, int viewType){
+            LayoutInflater layoutInflater = LayoutInflater.from(getActivity());
+            View view = layoutInflater
+                    .inflate(android.R.layout.simple_expandable_list_item_1, parent, false);
+            return new ActivityHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(ActivityHolder activityHolder, int position){
+            ResolveInfo resolveInfo = mActivities.get(position);
+            activityHolder.binActivity(resolveInfo);
+        }
+
+        @Override
+        public int getItemCount(){
+            return mActivities.size();
+        }
     }
 }
